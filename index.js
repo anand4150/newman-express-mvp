5a23f39e8da7a9deda3e50386d629bd804bec981
anand4150 <143164457+anand4150@users.noreply.github.com>
2025-03-01 07:25:18 +0000

    Updated port definition, will use 3000 as default

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Install dependencies: npm install express cors
+// Install dependencies: npm install express
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
